6b5ed3846ff422401f9c317bfce7df9f5dfbc4b8
赵志伟 <44900340+sdwurg180507280211@users.noreply.github.com>
2026-09-11 16:12:16 +0800

    feat: localize editor interface in Chinese

--- editor/src/main.tsx
+++ editor/src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import {App} from './App';
+import {enableChineseEditorUi} from './locale.zh-CN';
 import './styles.css';
+
+enableChineseEditorUi();
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
